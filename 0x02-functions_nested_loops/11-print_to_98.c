d6e223d1101387d2ddffa3db84234ebf993cb27b
IV-Codes <sadiqabdullahibashir@gmail.com>
2022-09-14 21:02:59 -0700

    loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,6 +6,7 @@
  * @n: starting integer
  * 
  * Description: prints all natural number from n - 98
+ * 
  * Return: void
  */
 
@@ -21,7 +22,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		while (n >98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
